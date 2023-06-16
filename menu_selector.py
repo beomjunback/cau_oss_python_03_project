8310ff38c1e73eea8c7d42df12cf69d7cbed6d00
beomjunback <baegdugi1004@gmail.com>
2023-06-16 20:47:10 +0900

    version2 complete

--- menu_selector.py
+++ menu_selector.py
@@ -1,14 +1,28 @@
+'''
+menu_selector 모듈은
+start_process 함수를 제공합니다. 이는
+메뉴에 표시된 원하는 옵션을 입력받고 실행합니다.
+현재 완성된 기능: "[1] print", "[4] exit"
+사용자가 1번을 택할시 path로 받은 정보를 모두 parking_spot_manager의 클래스의 형식에 맞게 출력합니다.
+사용자가 4번을 택할시 프로그램을 종료합니다. 그전까지 반복합니다.
+'''    
+
+import parking_spot_manager
+import file_manager
+
 def start_process(path):
+    
+    got = parking_spot_manager.str_list_to_class_list(file_manager.read_file(path))
+    
     while True:
-        print("---menu---")
+        print("---menu---") #메뉴를 표시합니다.
         print("[1] print")
         print("[2] filter")
         print("[3] sort")
         print("[4] exit")
         select = int(input('type:'))
-        if select == 1:
-            print("not implemented yet")
-            # fill this block
+        if select == 1: #1번 선택시 전부 출력합니다.
+            parking_spot_manager.print_spots(got)
         elif select == 2:
             print("---filter by---")
             print("[1] name")
@@ -51,8 +65,8 @@ def start_process(path):
                 print("not implemented yet")
                 # fill this block
             else: print("invalid input")
-        elif select == 4:
-            print("not implemented yet")
-            # fill this block
+        elif select == 4: #4번 선택시 프로그램을 종료합니다.
+            print("Exit")
+            break
         else:
             print("invalid input")
--- parking_spot_manager.py
+++ parking_spot_manager.py
@@ -1,21 +1,63 @@
+'''
+parking_spot_manager모듈은
+parking_spot 클래스를 제공합니다.
+생성자, get메소드, __str__특수 메소드를 제공합니다.
+그 외에 str_list_to_class_list 함수는 str형식의 리스트를 객체 리스트로 저장하여 반환합니다.
+print_spots 함수는 매게변수로 받은 리스트의 인덱스를 하나씩 전부 출력합니다.
+'''
 class parking_spot:
-    # you have to implement 'constructor(생성자)' and 'get' method
+    __item = {} #딕셔너리 생성
+    
+    def __init__(self,name,city,distict,ptype,longitude,latitude):
+        self.__item =\
+            {
+             'name':str(name),
+             'city':str(city),
+             'district':str(distict),
+             'ptype':str(ptype),
+             'longitude':float(longitude),
+             'latitude':float(latitude)
+            }
+        
+    def get(self,keyword='name'): #__item의 value를 반환
+        return self.__item[keyword]
+    
     def __str__(self):
         item = self.__item
         s  = f"[{item['name']}({item['ptype']})] "
         s += f"{item['city']} {item['district']}"
         s += f"(lat:{item['latitude']}, long:{item['longitude']})"
         return s
+    
+def str_list_to_class_list(str_list): #객체 생성
+    list_allinfo =[]
+    for i in str_list:
+        info = i.split(",")
+        name = info[1]
+        city = info[2]
+        district = info[3]
+        ptype = info[4]
+        longitude = info[5]
+        latitude = info[6]
+        spot = parking_spot(name,city,district,ptype,longitude,latitude)
+        list_allinfo.append(spot)
+    return list_allinfo
+
+def print_spots(spots): #출력
+    print(f"---print elements({len(spots)})---")
+    for i in spots:
+        print(i)
+        
 
 
 # 각 단계별로 테스트 (테스트할때 주석해제 후 사용)
 if __name__ == '__main__':
     print("Testing the module...")
     # version#2
-    # import file_manager
-    # str_list = file_manager.from_file("./input/free_parking_spot_seoul.csv")
-    # spots = str_list_to_class_list(str_list)
-    # print_spots(spots)
+    import file_manager
+    str_list = file_manager.read_file("./input/free_parking_spot_seoul.csv")
+    spots = str_list_to_class_list(str_list)
+    print_spots(spots)
 
     # version#3
     # spots = filter_by_district(spots, '동작')
